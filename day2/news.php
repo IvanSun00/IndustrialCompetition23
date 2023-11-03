b07f2545d5d8609b6e7be66b3f36215c11907fe0
JevelynC <jcal48072@gmail.com>
2023-11-03 18:32:10 +0700

    news

--- day2/news.php
+++ day2/news.php
@@ -219,17 +219,32 @@ if(!isset($_SESSION['nama_kelompok']) || $_SESSION['nama_kelompok'] == ""){
                 </nav>
             </div>
             <div style="margin-left: 2vw; margin-right: 2vw; position: relative" class="p-2">
-                <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-
+                <!-- <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4"> -->
+                <div>
                     <?php
-                    $sql = "SELECT * 
+                        $sql = "SELECT * 
                         FROM day2_news N
-                        JOIN day2_day D ON N.day = D.day";
+                        ORDER BY N.day DESC";
                     $result = $con->query($sql);
+                    $previousDay = 0; // Membuat variabel untuk melacak hari sebelumnya
                     while ($row = $result->fetch_assoc()) {
+                        $currentDay = $row['day'];
+                        // Memeriksa jika hari saat ini berbeda dari hari sebelumnya
+                        if ($currentDay != $previousDay) {
+                            // Tutup baris sebelumnya (jika ada)
+                            if ($previousDay != 0) {
+                                echo '</div>';
+                            }
+                            // Mulai baris baru
+                            ?>
+                            <div class="row text-center ">
+                                <h3 class="text-white col-12">DAY <?php echo $row['day'] ?></h3>
+                            <!-- </div> -->
+                            <?php
+                        }
                     ?>
-                        <div class="col">
-                            <div class="card mb-3" style="max-width: 540px;">
+                        <div class="col-12 col-md-4">
+                            <div class="card mb-3 mx-auto mx-md-0 " style="max-width: 540px;">
                                 <div class="row g-0">
                                     <!-- <div class="col-md-4">
                                 <img src="..." class="img-fluid rounded-start" alt="...">
@@ -244,6 +259,8 @@ if(!isset($_SESSION['nama_kelompok']) || $_SESSION['nama_kelompok'] == ""){
                             </div>
                         </div>
                     <?php
+                        $previousDay = $currentDay;
+
                     }
                     ?>
                 </div>
