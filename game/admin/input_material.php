47d68913dfb743c2fb0768a4267dc6da1d1b584f
FarrellYunanto <128081392+FarrellYunanto@users.noreply.github.com>
2023-11-07 11:15:24 +0700

    Update delay

--- game/admin/input_material.php
+++ game/admin/input_material.php
@@ -1,7 +1,7 @@
 <?php
 include '../../connect.php';
 $error_mess = false;
-if(!isset($_SESSION['nrp_admin']) || $_SESSION['nrp_admin'] == ""){
+if (!isset($_SESSION['nrp_admin']) || $_SESSION['nrp_admin'] == "") {
     header("Location: index.php");
     exit();
 }
@@ -19,12 +19,15 @@ $listKelompok = $query->fetchAll();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.rtl.min.css" rel="stylesheet" integrity="sha384-PRrgQVJ8NNHGieOA1grGdCTIt4h21CzJs6SnWH4YMQ6G5F5+IEzOHz67L4SQaF0o" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.rtl.min.css" rel="stylesheet"
+        integrity="sha384-PRrgQVJ8NNHGieOA1grGdCTIt4h21CzJs6SnWH4YMQ6G5F5+IEzOHz67L4SQaF0o" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/css/bootstrap-select.min.css">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/css/bootstrap-select.min.css">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+        </script>
 
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js">
     </script>
@@ -33,22 +36,59 @@ $listKelompok = $query->fetchAll();
     </script>
 
     <link rel="stylesheet" href="asset/style_input.css">
-    
+
     <!-- fav icon -->
     <link rel="icon" type="image/png" href="../../assets/logo%20ic.png">
+
+    <!-- Sweet Alert -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <title>Input Material</title>
 </head>
 
 <body>
+    <?php
+    if (isset($_SESSION['delay']) && $_SESSION['delay'] == true) {
+        ?>
 
+        <script>
+            let timerInterval;
+            Swal.fire({
+                title: "Please wait ^^",
+                html: "I will close in <b></b> milliseconds.",
+                timer: 2000,
+                timerProgressBar: true,
+                allowOutsideClick: false,
+                didOpen: () => {
+                    Swal.showLoading();
+                    const timer = Swal.getPopup().querySelector("b");
+                    timerInterval = setInterval(() => {
+                        timer.textContent = `${Swal.getTimerLeft()}`;
+                    }, 100);
+                },
+                willClose: () => {
+                    clearInterval(timerInterval);
+                }
+            }).then((result) => {
+                /* Read more about handling dismissals below */
+                if (result.dismiss === Swal.DismissReason.timer) {
+                    console.log("I was closed by the timer");
+                }
+            });
+        </script>
+
+        <?php
+        $_SESSION['delay'] = false;
+    }
+    ?>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <a class="navbar-brand col-2" href="#">
                 <img src="asset/img/logo_petra.jpg" alt="" width="55" height="55">
                 <span style="padding-left: 10px;">Input Website</span>
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -140,7 +180,9 @@ $listKelompok = $query->fetchAll();
                 <select class="selectpicker" id="fruitSelect" name="fruitselectmenang" data-live-search="true">
                     <option selected>Pilih kelompok</option>
                     <?php foreach ($listKelompok as $value) { ?>
-                        <option value="<?= $value['nama'] ?>"><?= $value['nama'] ?></option>
+                        <option value="<?= $value['nama'] ?>">
+                            <?= $value['nama'] ?>
+                        </option>
                     <?php } ?>
                 </select>
 
@@ -162,7 +204,8 @@ $listKelompok = $query->fetchAll();
 
 
                 <span class="btn">
-                    <input class="btn btn-primary" value="submit" type="submit" id="submitmenang" name="submitmenang">
+                    <input class="btn btn-primary" value="submit" type="submit" id="submitmenang" name="submitmenang"
+                        onclick="submit()">
                 </span>
 
             </form>
@@ -180,7 +223,9 @@ $listKelompok = $query->fetchAll();
                 <select class="selectpicker" id="fruitSelect" name="fruitselectseri" data-live-search="true">
                     <option selected>Pilih kelompok</option>
                     <?php foreach ($listKelompok as $value) { ?>
-                        <option value="<?= $value['nama'] ?>"><?= $value['nama'] ?></option>
+                        <option value="<?= $value['nama'] ?>">
+                            <?= $value['nama'] ?>
+                        </option>
                     <?php } ?>
                 </select>
 
@@ -200,7 +245,8 @@ $listKelompok = $query->fetchAll();
                 } ?>
 
                 <span class="btn">
-                    <button class="btn btn-primary" type="submit" value="submit" id="submitseri" name="submitseri">Submit</button>
+                    <button class="btn btn-primary" type="submit" value="submit" id="submitseri"
+                        name="submitseri">Submit</button>
                 </span>
             </form>
         </div>
@@ -217,7 +263,9 @@ $listKelompok = $query->fetchAll();
                 <select class="selectpicker" id="fruitSelect" name="fruitselectkalah" data-live-search="true">
                     <option selected>Pilih kelompok</option>
                     <?php foreach ($listKelompok as $value) { ?>
-                        <option value="<?= $value['nama'] ?>"><?= $value['nama'] ?></option>
+                        <option value="<?= $value['nama'] ?>">
+                            <?= $value['nama'] ?>
+                        </option>
                     <?php } ?>
                 </select>
 
@@ -238,19 +286,26 @@ $listKelompok = $query->fetchAll();
 
 
                 <span class="btn">
-                    <button class="btn btn-primary" type="submit" value="submit" id="submitkalah" name="submitkalah">Submit</button>
+                    <button class="btn btn-primary" type="submit" value="submit" id="submitkalah"
+                        name="submitkalah">Submit</button>
                 </span>
             </form>
         </div>
 
     </div>
 
+    <script>
+        function submit() {
+
+        }
+    </script>
     <!-- Tambahkan link ke jQuery (diperlukan oleh Bootstrap dan Bootstrap-select) -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- Tambahkan link ke Bootstrap JS (diperlukan oleh Bootstrap-select) -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/5.0.0/js/bootstrap.min.js"></script>
     <!-- Tambahkan link ke Bootstrap-select JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/js/bootstrap-select.min.js"></script>
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/js/bootstrap-select.min.js"></script>
 </body>
 
 </html>
