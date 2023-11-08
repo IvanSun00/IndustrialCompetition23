7698c880b539e4130eeb876290612717d383bdcf
Jovan Andhika <c14220013@john.petra.ac.id>
2023-11-08 07:10:14 +0700

    attach swal

--- day2/admin/input_sertifikasi/index.php
+++ day2/admin/input_sertifikasi/index.php
@@ -59,6 +59,9 @@ if(isset($_SESSION['namekel_day2'])){
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
 
+    <!-- Sweet Alert -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js">
     </script>
 
